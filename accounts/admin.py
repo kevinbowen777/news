e434380970473149189ff0c3020c292813dfe48d
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-26 08:54:10 -0800

    Add fieldsets for CustomUser modifications

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
@@ -8,6 +9,13 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ['email', 'username', 'age', 'is_staff', ]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
