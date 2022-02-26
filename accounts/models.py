118baa3bdc0ec0f3e385baf94f4f8aac8ae1d750
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-26 08:44:40 -0800

    Add CustomUser model to db

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class CustomerUser(AbstractUser):
+class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
