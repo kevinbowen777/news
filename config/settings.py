f519729bb076bd412b127b33039ecc37b5fe69d1
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-28 15:59:14 -0800

    Setup DATABASE_URL for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -84,13 +84,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": env.dj_db_url("DATABASE_URL")
+
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
