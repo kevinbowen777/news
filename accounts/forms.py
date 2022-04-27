30308d0f3fbe5cfe8dbde35f6a0ca9bd5df840b5
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-27 00:37:51 -0700

    Add tests for user creation & signup page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-
-from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
-        model = CustomUser
+        model = get_user_model()
         fields = (
             "username",
             "email",
@@ -15,7 +14,7 @@ class CustomUserCreationForm(UserCreationForm):
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = (
             "username",
             "email",
