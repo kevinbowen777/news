32a33b2c27d9cbe3ec4a862f152e70dda7d976a4
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-18 14:29:17 -0700

    Create CustomUser Update & Create forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,22 +1,39 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.core.exceptions import ValidationError
 
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = get_user_model()
-        fields = (
-            "username",
-            "email",
-            "age",
-        )
+User = get_user_model()
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = (
             "username",
             "email",
             "age",
         )
+
+
+class CustomUserCreationForm(UserCreationForm):
+    error_message = UserCreationForm.error_messages.update(
+        {"duplicate_username": "This username has already been taken."}
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+
+        raise ValidationError(self.error_messages["duplicate_username"])
