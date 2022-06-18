c821ac5f58299c2f175741cbe0fcce946f7e2b11
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-18 14:30:37 -0700

    Wire in profile URLs to views

--- config/urls.py
+++ config/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     # Local apps
     path("articles/", include("articles.urls")),
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
