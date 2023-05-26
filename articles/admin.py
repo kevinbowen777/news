b6a2fbd5dde19fc0d5087b1704d103a1b1f1707c
Kevin Bowen <kevin.bowen@gmail.com>
2023-05-26 15:03:03 -0700

    Add list_display, filter, search...Resolves #260

--- articles/admin.py
+++ articles/admin.py
@@ -7,11 +7,19 @@ class CommentInline(admin.TabularInline):
     model = Comment
 
 
+@admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    ordering = ["status", "publish"]
     inlines = [
         CommentInline,
     ]
 
 
-admin.site.register(Article, ArticleAdmin)
+# admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
