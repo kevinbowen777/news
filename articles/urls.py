352d1fe1a0174f9a4338211ea378d25d8a7664cd
Kevin Bowen <kevin.bowen@gmail.com>
2023-05-26 19:04:01 -0700

    Add YMD Slug path

--- articles/urls.py
+++ articles/urls.py
@@ -14,10 +14,12 @@ from .views import (
 
 urlpatterns = [
     path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
-    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
     path(
-        "<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"
+        "<int:year>/<int:month>/<int:day>/<slug:slug>/",
+        ArticleDetailView.as_view(),
+        name="article_detail",
     ),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
     path("new/", ArticleCreateView.as_view(), name="article_new"),
     path("", ArticleListView.as_view(), name="article_list"),
     path(
