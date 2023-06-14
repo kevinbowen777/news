56e78ea86c900c39946d0212b17c0fc9c4c171dc
Kevin Bowen <kevin.bowen@gmail.com>
2023-06-14 15:47:53 -0700

    Add list of similar articles to article detail page. Resolves #267

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views.decorators.http import require_POST
@@ -43,10 +44,23 @@ def article_detail(request, year, month, day, article):
     comments = article.comments.filter(active=True)
     form = CommentForm()
 
+    article_tags_ids = article.tags.values_list("id", flat=True)
+    similar_articles = Article.published.filter(tags__in=article_tags_ids).exclude(
+        id=article.id
+    )
+    similar_articles = similar_articles.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
+
     return render(
         request,
         "articles/article_detail.html",
-        {"article": article, "comments": comments, "form": form},
+        {
+            "article": article,
+            "comments": comments,
+            "form": form,
+            "similar_articles": similar_articles,
+        },
     )
 
 
