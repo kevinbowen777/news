2eb19e4f06cb0e67365cef7e2d11cbc1a7648cf2
Kevin Bowen <kevin.bowen@gmail.com>
2023-06-16 17:55:49 -0700

    Add support for Postgres full-text search(#276)

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "django.contrib.sitemaps",
+    "django.contrib.postgres",
     # Third-party applications
     "allauth",
     "allauth.account",
