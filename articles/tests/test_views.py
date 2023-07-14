7e14ba3f83c2071777e0679bc61ced8e3e463dd2
Kevin Bowen <kevin.bowen@gmail.com>
2023-07-14 00:56:19 -0700

    Cleanup page tests & reformat templates. Resolves #310

--- articles/tests/test_views.py
+++ articles/tests/test_views.py
@@ -20,6 +20,14 @@ class ArticleTests(TestCase):
             author=self.user,
         )
 
+        self.article2 = Article.objects.create(
+            title="A good second title",
+            body="Nice body content for a second article",
+            # slug="a-good-title",
+            author=self.user,
+            status="DF",
+        )
+
     def test___str__(self):
         assert self.article.__str__() == self.article.title
         assert str(self.article) == self.article.title
@@ -29,22 +37,21 @@ class ArticleTests(TestCase):
         self.assertEqual(f"{self.article.slug}", "a-good-title")
         self.assertEqual(f"{self.article.author}", "johndoe")
         self.assertEqual(f"{self.article.body}", "Nice body content")
+        self.assertEqual(f"{self.article.status}", "PB")
 
-    """
     def test_get_absolute_url(self):
         self.assertEqual(
-            self.article.get_absolute_url(), "/articles/{self.article.id}/")
-        # self.assertEqual(self.article.get_absolute_url(), "/articles/1/")
+            self.article.get_absolute_url(), f"/articles/2023/7/14/{self.article.slug}/"
+        )
 
     def test_article_detail_view(self):
         self.client.login(email="johndoe@example.com", password="secret")
-        response = self.client.get("/articles/{self.article.id}/")
+        response = self.client.get(f"/articles/2023/7/14/{self.article.slug}/")
         # no_response = self.client.get("articles/100000/")
         self.assertEqual(response.status_code, 200)
         # self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, "articles/article_detail.html")
-    """
 
     def test_article_create_view(self):
         self.client.login(email="johndoe@example.com", password="secret")
@@ -81,7 +88,6 @@ class ArticleTests(TestCase):
 
 
 class ArticleListViewTest(TestCase):
-    """
     def setUp(self):
         url = reverse("article_list")
         self.response = self.client.get(url)
@@ -98,27 +104,18 @@ class ArticleListViewTest(TestCase):
             slug="a-good-title",
             author=self.user,
         )
-    """
 
-    def setUp(self):
-        url = reverse("article_list")
-        self.response = self.client.get(url)
-
-        self.user = get_user_model().objects.create_user(
-            username="johndoe",
-            email="johndoe@example.com",
-            password="secret",
-        )
-
+        """
         # Create articles for pagination tests
         number_of_articles = 10
         for article_id in range(number_of_articles):
             Article.objects.create(
                 title="A Tiny Test Article {0}".format(article_id),
-                slug="a-tiny-test-article {0}".format(article_id),
+                slug="2023/7/15/a-tiny-test-article-{0}/".format(article_id),
                 body="Some article content {0}".format(article_id),
                 author=self.user,
             )
+        """
 
     def test_view_url_exists_at_desired_location(self):
         # response = self.client.get("/articles/")
@@ -132,13 +129,14 @@ class ArticleListViewTest(TestCase):
         self.assertTemplateUsed(self.response, "articles/article_list.html")
 
     """
-    def test_pagination_is_three(self):
+    def test_pagination_is_five(self):
         self.assertEqual(self.response.status_code, 200)
         self.assertTrue("is_paginated" in self.response.context)
         self.assertTrue(self.response.context["is_paginated"] is True)
-        self.assertEqual(len(self.response.context["article_list"]), 3)
+        self.assertEqual(len(self.response.context["article_list"]), 5)
 
     def test_lists_all_articles(self):
+        self.client.login(email="johndoe@example.com", password="secret")
         # Get second page and confirm it has (exactly) the remaining 3 items
         response = self.client.get("articles/?page=2")
         self.assertEqual(self.response.status_code, 200)
@@ -177,30 +175,31 @@ class CommentTests(TestCase):
             {
                 "name": "John Doe",
                 "email": "johndoe@example.com",
-                "body": "This is a comment",
+                "body": "This is a new comment",
                 "active": True,
             },
         )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(Comment.objects.first().body, "This is a comment")
+        self.assertEqual(Comment.objects.last().body, "This is a new comment")
+        # self.assertEqual(Comment.objects.first().body, "This is a new comment")
         # self.assertTrue(self.comment.name == self.user)
 
     """
     def test_comment_update(self):
         self.client.login(email="johndoe@example.com", password="secret")
         response = self.client.post(
-            reverse("comment_edit", args={self.comment.id}),
+            reverse("comment_edit", args={self.article.id}),
             {
                 "body": "Updated comment",
             },
         )
-        self.assertEqual(response.status_code, 200)
-        # self.assertEqual(Comment.objects.first().comment, "Updated comment")
+        # self.assertEqual(response.status_code, 200)
+        self.assertEqual(Comment.objects.last().body, "Updated comment")
 
     def test_comment_delete(self):
         self.client.login(email="johndoe@example.com", name="John Doe", password="secret")
         response = self.client.post(
-            reverse("comment_delete", args={self.comment.id}),
+            reverse("comment_delete", args={self.article.id}),
         )
         self.assertEqual(response.status_code, 302)
         # self.assertNotContains(Message.objects.all().text, "Updated title")
@@ -209,3 +208,22 @@ class CommentTests(TestCase):
     def test___str__(self):
         assert self.comment.__str__() == "Comment by John Doe on A good title"
         # assert str(self.comment) == self.comment.body
+
+
+class SitemapTests(TestCase):
+    def setUp(self):
+        # url = reverse("sitemap")
+        url = "/sitemap.xml"
+        self.response = self.client.get(url)
+
+    def test_view_url_exists_at_desired_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+
+class RSSFeedTests(TestCase):
+    def setUp(self):
+        url = reverse("article_feed")
+        self.response = self.client.get(url)
+
+    def test_feed_url_exists_at_desired_location(self):
+        self.assertEqual(self.response.status_code, 200)
