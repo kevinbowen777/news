f976a4793e436e6fbdb1d463b9c90f9905e6253a
Kevin Bowen <kevin.bowen@gmail.com>
2024-02-05 15:02:21 -0800

    Update fieldsets. Resolves #326 (#420)

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,26 +4,46 @@ from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 
-CustomUser = get_user_model()
+User = get_user_model()
 
 
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = CustomUser
+    model = User
     list_display = [
         "email",
         "username",
-        "age",
         "is_staff",
     ]
-    fieldsets = UserAdmin.fieldsets
-    fieldsets[1][1]["fields"] = fieldsets[1][1]["fields"] + (
-        "age",
-        "country",
-        "profile_pic",
-        "bio",
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "age",
+                    "country",
+                    "profile_pic",
+                    "bio",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
